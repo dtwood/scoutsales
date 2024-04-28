0b42113d5d7eeef96c640d23cabc3bc3959c2ed9
David Wood <code@dtwood.co.uk>
2024-04-28 16:10:01 +0100

    Add admin action to fix scout sold items

--- scoutsalesapp/admin.py
+++ scoutsalesapp/admin.py
@@ -16,8 +16,14 @@ class TransactionAdmin(admin.ModelAdmin):
 admin.site.register(Transaction, TransactionAdmin)
 
 
+@admin.action(description="Make scouts")
+def make_published(modeladmin, request, queryset):
+    queryset.update(seller_email="scouts@newnhamscouts.org.uk", donation=100, seller_name="Scout Group")
+
+
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['slug', 'seller_name', 'title', 'price', 'donation']
+    actions = [make_published]
 
 
 admin.site.register(Item, ItemAdmin)
